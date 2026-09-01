b3c2a479853072d62ca29a198d5290a3bcafad8c
daksh231628 <daksh231628@gmail.com>
2026-09-01 14:18:07 +0530

    Add data types practice examples in data_types_practice.py
    This file contains various examples of Python data types and their usage.

---
+++ Python/Assignment-3/data_types_practice.py
@@ -0,0 +1,113 @@
+# #Q18.
+# x = 25
+# print(type(x))
+
+# #Q19.
+# price = 99.50
+# print(type(price))
+# #Q20.
+# name = "Python"
+# print(type(name))
+# #Q21.
+# is_active = True
+# print(type(is_active))
+
+# #Q22.
+# data = None
+# print(type(data))
+# #Q23.
+# number = 25
+# price = 99.50
+# name = "Python"
+# is_active = True
+# data = None
+
+# print(type(number))
+# print(type(price))
+# print(type(name))
+# print(type(is_active))
+# print(type(data))
+# #Q24.
+# student_name = "Ayush"
+# student_age = 18
+# student_height = 5.8
+# is_student = True
+# student_result = None
+
+# print(type(student_name))
+# print(type(student_age))
+# print(type(student_height))
+# print(type(is_student))
+# print(type(student_result))
+
+
+# #Q25.
+# a = 50
+# b = 50.0
+# c = "50"
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+# #Q26.
+# a = True
+# b = "True"
+
+# print(type(a))
+# print(type(b))
+
+
+# #Q27.
+
+# a = None
+# b = "None"
+
+# print(type(a))
+# print(type(b))
+
+
+# #Q28.
+
+# value = 100
+# print(type(value))
+
+# value = "Python"
+# print(type(value))
+
+
+# #Q29.
+
+# product_name = "Laptop"
+# product_quantity = 10
+# product_price = 59999.50
+# product_available = True
+# product_discount = None
+
+# print(type(product_name))
+# print(type(product_quantity))
+# print(type(product_price))
+# print(type(product_available))
+# print(type(product_discount))
+
+
+# #Q30.
+# a = 10
+# b = 10.0
+# c = "10"
+# d = True
+# e = "True"
+# f = None
+# g = "None"
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+
+
+
